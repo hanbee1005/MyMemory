2a2d60ce5dd7fceb5bc3404fe4229d9ea31a5478
hanbee <sonhanbi1002@gmail.com>
2021-07-26 10:51:38 +0900

    메모 셀 생성 및 스토리보드 연결

---
+++ MyMemory/MemoCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoCell.swift
+//  MyMemory
+//
+//  Created by 손한비 on 2021/07/26.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
