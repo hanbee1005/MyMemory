dc0da87995dc72369294df3de5c20db30c829998
hanbee <sonhanbi1002@gmail.com>
2021-07-26 15:34:33 +0900

    메모 상세보기 화면 구성 및 받은 데이터 보여주기

--- MyMemory/MemoReadVC.swift
+++ MyMemory/MemoReadVC.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 class MemoReadVC: UIViewController {
+    
+    // 콘텐츠 데이터를 저장하는 변수
+    var param : MemoData?
 
+    @IBOutlet var subject: UILabel!
+    @IBOutlet var contents: UILabel!
+    @IBOutlet var img: UIImageView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // 제목과 내용, 이미지를 출력
+        self.subject.text = param?.title
+        self.contents.text = param?.contents
+        self.img.image = param?.image
+        
+        // 날짜 포맷 변환
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd일 HH:mm분에 작성됨"
+        let dateString = formatter.string(from: (param?.regdate)!)
+        
+        // 내비게이션 타이틀에 날짜를 표시
+        self.navigationItem.title = dateString
     }
     
 
