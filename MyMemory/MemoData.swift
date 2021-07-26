3d185b6f09edf8509da00667a672c7bd3118a3de
hanbee <sonhanbi1002@gmail.com>
2021-07-26 11:00:53 +0900

    데이터 모델 생성

---
+++ MyMemory/MemoData.swift
@@ -0,0 +1,16 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by 손한비 on 2021/07/26.
+//
+
+import UIKit
+
+class MemoData {
+    var memoId : Int? // 데이터 식별값
+    var title : String? // 메모 제목
+    var contents : String? // 메모 내용
+    var image : UIImage? // 이미지
+    var regdate : Date? // 작성일
+}
