674cd4569e73984a310c9a465bfdf556de7285f8
hanbee <sonhanbi1002@gmail.com>
2021-07-26 15:38:00 +0900

    메모 리스트 뷰에서 메모 선택시 메모 데이터 전달 기능 구현

--- MyMemory/MemoListVC.swift
+++ MyMemory/MemoListVC.swift
@@ -53,6 +53,16 @@ class MemoListVC: UITableViewController {
     // MARK: - table view delegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        <#code#>
+        // memolist 배열에서 선택된 행에 맞는 데이터를 꺼낸다.
+        let row = self.appDelegate.memolist[indexPath.row]
+        
+        // 상세 화면의 인스턴스를 생성한다.
+        guard let vc = self.storyboard?.instantiateViewController(identifier: "MemoRead") as? MemoReadVC else {
+            return
+        }
+        
+        // 값을 전달한 다음, 상세 화면으로 이동
+        vc.param = row
+        self.navigationController?.pushViewController(vc, animated: true)
     }
 }
