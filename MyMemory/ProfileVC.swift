b6920f94a270e6f2ddc0f192ff7cbf938cb96fb9
hanbee <sonhanbi1002@gmail.com>
2021-08-20 15:36:43 +0900

    ProfileVC 생성 및 UserInfoManager 멤버 상수 선언

---
+++ MyMemory/ProfileVC.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileVC.swift
+//  MyMemory
+//
+//  Created by 손한비 on 2021/08/20.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController, UITableViewDelegate {
+    let uinfo = UserInfoManager()  // 개인 정보 관리 매니저
+}
