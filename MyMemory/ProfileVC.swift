72202c3f201a0064eae71a852a8662519a76de15
hanbee <sonhanbi1002@gmail.com>
2021-08-20 15:46:22 +0900

    ProfileVC에서 로그인 처리 메소드 구현 (로그인 성공 시 기능 구현 x)

--- MyMemory/ProfileVC.swift
+++ MyMemory/ProfileVC.swift
@@ -9,4 +9,36 @@ import UIKit
 
 class ProfileVC: UIViewController, UITableViewDelegate {
     let uinfo = UserInfoManager()  // 개인 정보 관리 매니저
+    
+    @objc func doLogin(_ sender: Any) {
+        let loginAlert = UIAlertController(title: "LOGIN", message: nil, preferredStyle: .alert)
+        
+        // 알림창에 들어갈 입력폼 추가
+        loginAlert.addTextField() { (tf) in
+            tf.placeholder = "Your Account"
+        }
+        
+        loginAlert.addTextField() { (tf) in
+            tf.placeholder = "Password"
+            tf.isSecureTextEntry = true
+        }
+        
+        // 알림창 버튼 추가
+        loginAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        loginAlert.addAction(UIAlertAction(title: "Login", style: .destructive) { (_) in
+            let account = loginAlert.textFields?[0].text ?? ""  // 첫번째 필드 : 계정
+            let passwd = loginAlert.textFields?[1].text ?? "" // 두번째 필드 : 비밀번호
+            
+            if self.uinfo.login(account: account, passwd: passwd) {
+                // 로그인 성공 시 처리할 내용
+            } else {
+                let msg = "로그인에 실패하였습니다."
+                let alert = UIAlertController(title: nil, message: msg, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.present(alert, animated: false, completion: nil)
+            }
+        })
+        
+        self.present(loginAlert, animated: false, completion: nil)
+    }
 }
