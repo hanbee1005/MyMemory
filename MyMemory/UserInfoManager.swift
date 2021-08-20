1f60d6716ec267a2e2604b7d2638b61064e3c0cb
hanbee <sonhanbi1002@gmail.com>
2021-08-20 15:02:55 +0900

    UserInfoManager 파일 생성 및 데이터를 저장할 키를 열거형 상수로 지정

---
+++ MyMemory/UserInfoManager.swift
@@ -0,0 +1,16 @@
+//
+//  UserInfoManager.swift
+//  MyMemory
+//
+//  Created by 손한비 on 2021/08/20.
+//
+
+import UIKit
+
+struct UserInfoKey {
+    // 저장에 사용할 키
+    static let loginId = "LOGINID"
+    static let account = "ACCOUNT"
+    static let name = "NAME"
+    static let profile = "PROFILE"
+}
