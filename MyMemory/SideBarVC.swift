8d4417301ecefe39cd01a26b32b9c8a9e33c7997
hanbee <sonhanbi1002@gmail.com>
2021-08-21 22:33:00 +0900

    메뉴 목록 및 아이콘 정의

--- MyMemory/SideBarVC.swift
+++ MyMemory/SideBarVC.swift
@@ -8,5 +8,34 @@
 import UIKit
 
 class SideBarVC: UITableViewController {
+    // 목록 데이터 배열
+    let titles = ["새글 작성하기", "친구 새글", "달력으로 보기", "공지사향", "통계", "계정 관리"]
     
+    // 아이콘 데이터 배열
+    let icons = [
+        UIImage(named: "icon01.png"),
+        UIImage(named: "icon02.png"),
+        UIImage(named: "icon03.png"),
+        UIImage(named: "icon04.png"),
+        UIImage(named: "icon05.png"),
+        UIImage(named: "icon06.png"),
+    ]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.titles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 재사용 큐에서 테이블 셀을 꺼내온다. 없으면 새로 생성한다.
+        let id = "menucell"  // 테이블 셀 식별자
+        let cell = tableView.dequeueReusableCell(withIdentifier: id) ?? UITableViewCell(style: .default, reuseIdentifier: id)
+        
+        // 타이틀과 이미지를 대입한다.
+        cell.textLabel?.text = self.titles[indexPath.row]
+        cell.imageView?.image = self.icons[indexPath.row]
+        
+        // 폰트 설정
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        return cell
+    }
 }
