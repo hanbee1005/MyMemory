d26eae9a60c47264cef33aecea6f134f696113db
hanbee <sonhanbi1002@gmail.com>
2021-08-22 21:27:23 +0900

    TutorialContentsVC 기본 필드 구성하기

---
+++ MyMemory/Tutorial/TutorialContentsVC.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialContentsVC.swift
+//  MyMemory
+//
+//  Created by 손한비 on 2021/08/22.
+//
+
+import UIKit
+
+class TutorialContentsVC: UIViewController {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var bgImageView: UIImageView!
+    
+    var pageIndex: Int!
+    var titleText: String!
+    var ImageFile: String!
+}
