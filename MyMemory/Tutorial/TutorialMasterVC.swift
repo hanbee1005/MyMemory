2ef79bff3d3645393e0a0f2f4430b15c3811f31b
hanbee <sonhanbi1002@gmail.com>
2021-08-22 21:34:13 +0900

    TutorialMasterVC 생성 및 UIPageViewController 참조 변수 추가

---
+++ MyMemory/Tutorial/TutorialMasterVC.swift
@@ -0,0 +1,12 @@
+//
+//  TutorialMasterVC.swift
+//  MyMemory
+//
+//  Created by 손한비 on 2021/08/22.
+//
+
+import UIKit
+
+class TutorialMasterVC: UIViewController {
+    var pageVC: UIPageViewController!
+}
