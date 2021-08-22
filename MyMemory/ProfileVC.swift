6ddaeb4e55d63969e5432b2adcac10a247c41856
hanbee <sonhanbi1002@gmail.com>
2021-08-22 17:33:40 +0900

    프로필 사진의 소스 타입을 선택하는 액션 메소드 구현

--- MyMemory/ProfileVC.swift
+++ MyMemory/ProfileVC.swift
@@ -117,6 +117,44 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource, U
         self.present(alert, animated: false, completion: nil)
     }
     
+    // 프로필 사진의 소스 타입을 선택하는 액션 메소드
+    @objc func profile(_ sender: UIButton) {
+        // 로그인 되어 있지 않을 경우에는 프로필 이미지 등록을 막고 대신 로그인 창을 띄워준다.
+        guard self.uinfo.account != nil else {
+            self.doLogin(self)
+            return
+        }
+        
+        let alert = UIAlertController(title: nil, message: "사진을 가져올 곳을 선택하세요.", preferredStyle: .actionSheet)
+        
+        // 카메라를 사용할 수 있으면
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            alert.addAction(UIAlertAction(title: "카메라", style: .default) { (_) in
+                self.imagePicker(.camera)
+            })
+        }
+        
+        // 저장된 앨범을 사용할 수 있으면
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
+            alert.addAction(UIAlertAction(title: "저장된 앨범", style: .default) { (_) in
+                self.imagePicker(.savedPhotosAlbum)
+            })
+        }
+        
+        // 포토 라이브러리를 사용할 수 있으면
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            alert.addAction(UIAlertAction(title: "포토 라이브러리", style: .default) { (_) in
+                self.imagePicker(.photoLibrary)
+            })
+        }
+        
+        // 취소 버튼 추가
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        
+        // 액션 시트 창 실행
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     func drawBtn() {
         // 버튼을 감쌀 뷰를 정의한다.
         let v = UIView()
