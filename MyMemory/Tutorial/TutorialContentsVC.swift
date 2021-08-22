ba49f54cfc178c73b482d945fc85df295c571a2a
hanbee <sonhanbi1002@gmail.com>
2021-08-22 21:29:54 +0900

    각 컨텐츠에 맞게 외부에서 전달받은 값을 아울렛 변수에 전달

--- MyMemory/Tutorial/TutorialContentsVC.swift
+++ MyMemory/Tutorial/TutorialContentsVC.swift
@@ -14,4 +14,13 @@ class TutorialContentsVC: UIViewController {
     var pageIndex: Int!
     var titleText: String!
     var ImageFile: String!
+    
+    override func viewDidLoad() {
+        // 전달받은 타이틀 정보를 레이블 객체에 대입하고 크기를 조절한다.
+        self.titleLabel.text = self.titleText
+        self.titleLabel.sizeToFit()
+        
+        // 전달받은 이미지 정보를 이미지 뷰에 대입한다.
+        self.bgImageView.image = UIImage(named: self.ImageFile)
+    }
 }
