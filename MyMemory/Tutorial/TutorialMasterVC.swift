b7beb8c9e596f65140eff0647c773c32f83d2a97
hanbee <sonhanbi1002@gmail.com>
2021-08-22 21:46:02 +0900

    인덱스에 맞는 컨텐츠 뷰를 생성하여 반환하는 메소드 구현

--- MyMemory/Tutorial/TutorialMasterVC.swift
+++ MyMemory/Tutorial/TutorialMasterVC.swift
@@ -7,6 +7,38 @@
 
 import UIKit
 
-class TutorialMasterVC: UIViewController {
+class TutorialMasterVC: UIViewController, UIPageViewControllerDataSource {
     var pageVC: UIPageViewController!
+    
+    // 콘텐츠 뷰 컨트롤러에 들어갈 티이틀과 이미지
+    var contentTitles = ["STEP1", "STEP2", "STEP3", "STEP4"]
+    var contentImages = ["Page0", "Page1", "Page2", "Page3"]
+    
+    func getContentVC(atIndex idx: Int) -> UIViewController? {
+        // 인덱스가 데이터 배열 크기 범위를 벗어나면 nil 반환
+        guard self.contentTitles.count >= idx && self.contentTitles.count > 0 else {
+            return nil
+        }
+        
+        // "ContentsVC" 라는 Storyboard ID를 가진 뷰 컨트롤러의 인스턴스를 생성하고 캐스팅한다.
+        guard let cvc = self.instanceTutorialVC(name: "ContentsVC") as? TutorialContentsVC else {
+            return nil
+        }
+        
+        // 컨텐츠 뷰 컨트롤러의 내용을 구성한다.
+        cvc.titleText = self.contentTitles[idx]
+        cvc.ImageFile = self.contentImages[idx]
+        cvc.pageIndex = idx
+        return cvc
+    }
+    
+    // MARK: - UIPageViewControllerDataSource
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        <#code#>
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        <#code#>
+    }
 }
