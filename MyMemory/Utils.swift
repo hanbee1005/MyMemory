4567074c7dacfac24bdfe15f371b9d5f134829df
hanbee <sonhanbi1002@gmail.com>
2021-08-27 17:31:58 +0900

    키 체인에서 값 읽어오는 메소드 구현

--- MyMemory/Utils.swift
+++ MyMemory/Utils.swift
@@ -26,6 +26,32 @@ class TokenUtils {
         assert(status == noErr, "토큰 값 저장에 실패했습니다.")
         NSLog("status=\(status)")
     }
+    
+    // 키 체인에 저장된 값을 읽어오는 메소드
+    func load(_ service: String, account: String) -> String? {
+        // 1. 키 체인 쿼리 정의
+        let keyChainQuery: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecReturnData: kCFBooleanTrue,
+            kSecMatchLimit: kSecMatchLimitOne
+        ]
+        
+        // 2. 키 체인에 저장된 값을 읽어온다.
+        var dataTypeRef: AnyObject?
+        let status = SecItemCopyMatching(keyChainQuery, &dataTypeRef)
+        
+        // 3. 처리 결과가 성공이라면 읽어온 값을 Data 타입으로 변환하고, 다시 String 타입으로 변환한다.
+        if (status == errSecSuccess) {
+            let retrievedData = dataTypeRef as! Data
+            let value = String(data: retrievedData, encoding: .utf8)
+            return value
+        } else {  // 처리 결과가 실패라면 nil을 반환한다.
+            print("Nothing was retrieved from keychain. Status code \(status)")
+            return nil
+        }
+    }
 }
 
 extension UIViewController {
